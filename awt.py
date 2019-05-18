d039f53e4f62e681f54591735523b1f69767a3cf
Nikhil Singh <nikhil.singh@sap.com>
2019-05-19 00:41:08 +0200

    Extracted chats with labels for all files

--- awt.py
+++ awt.py
@@ -5,37 +5,52 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 from xml.dom import minidom
+import xml
+import glob
 
 print(np.__version__)
 print(tf.__version__)
 
+path = 'BayzickBullyingData/Human Concensus/'
 
-# To extract the chat with labels
-df = pd.read_excel(r'BayzickBullyingData/Human Concensus/Packet1Concensus.xlsx')
-files = np.array(df['Bully Tracer Consensus'])
-labels = np.array(df['Unnamed: 1'])
-files_to_labels = dict(zip(files[2:], labels[2:]))
 
+# To extract the file names with labels
+def extract_files_labels(file):
+    df = pd.read_excel(file)
+    files = np.array(df['Bully Tracer Consensus'], dtype='string')
+    labels = np.array(df['Unnamed: 1'], dtype='string')
+    files_to_labels = dict(zip(files[2:], labels[2:]))
+    return files_to_labels
+
+
+# To extract the chats with labels
+result = [extract_files_labels(files) for files in glob.glob(path + "*")]
 data1 = list()
 lbls = list()
-for i in files_to_labels.keys():
-    try:
-        doc = minidom.parse("BayzickBullyingData/xml packet 1/" + i + ".xml")
-        name = doc.getElementsByTagName("body")
-        data = list()
-        for j in name:
-            try:
-                data.append(j.firstChild.data)
-            except AttributeError:
-                print("***************************object has no attribute 'data'*****************************")
-        res = [''.join(data)]
-        data1.append(res)
-        lbls.append(files_to_labels[i])
-    except IOError:
-        print("######################No such file {}".format("BayzickBullyingData/xml packet 1/" + i + ".xml") +
-              "##############################")
+for i in result:
+    for j in i.keys():
+        try:
+            doc = minidom.parse("BayzickBullyingData/packet-all/" + j + ".xml")
+            name = doc.getElementsByTagName("body")
+            data = list()
+            for k in name:
+                try:
+                    data.append(k.firstChild.data)
+                except AttributeError:
+                    pass
+                    #print("***************************object has no attribute 'data'*****************************")
+            res = [''.join(data)]
+            data1.append(res)
+            lbls.append(i[j])
+        except IOError:
+            pass
+            #print("######################No such file {}".format("BayzickBullyingData/packet-all/" + j + ".xml") +
+            #      "##############################")
+        except xml.parsers.expat.ExpatError:
+            pass
+            #print("Not well formed: {}".format(j + ".xml"))
 flat_list = [item for x in data1 for item in x]
 chat_with_labels = dict(zip(flat_list, lbls))
-
-print(chat_with_labels)
+for k, v in chat_with_labels.items():
+    print(k, v)
 print(len(chat_with_labels))
