8ffefc0234d2697356345e3e0c59c13828d46ca6
Nikhil Singh <nikhil.singh@sap.com>
2019-05-25 21:47:51 +0200

    Trained and tested RNN model using softmax prediction layer

--- awt.py
+++ awt.py
@@ -26,7 +26,7 @@ def extract_files_labels(file):
 # To extract the chats with labels
 result = [extract_files_labels(files) for files in glob.glob(path + "*")]
 data1 = list()
-lbls = list()
+labels = list()
 for i in result:
     for j in i.keys():
         try:
@@ -41,7 +41,7 @@ for i in result:
                     #print("***************************object has no attribute 'data'*****************************")
             res = [''.join(data)]
             data1.append(res)
-            lbls.append(i[j])
+            labels.append(i[j])
         except IOError:
             pass
             #print("######################No such file {}".format("BayzickBullyingData/packet-all/" + j + ".xml") +
@@ -49,8 +49,115 @@ for i in result:
         except xml.parsers.expat.ExpatError:
             pass
             #print("Not well formed: {}".format(j + ".xml"))
-flat_list = [item for x in data1 for item in x]
-chat_with_labels = dict(zip(flat_list, lbls))
-for k, v in chat_with_labels.items():
-    print(k, v)
-print(len(chat_with_labels))
+conversation = [item for x in data1 for item in x]
+chat_with_labels = dict(zip(conversation, labels))
+#for k, v in chat_with_labels.items():
+    #print(k, v)
+#print(len(chat_with_labels))
+
+# To convert labels from Yes, No to 1, 0
+for n, i in enumerate(labels):
+    if i == 'N':
+        labels[n] = 0
+    elif i == 'Y':
+        labels[n] = 1
+#print(lbls[:5])
+#print(flat_list[:5])
+
+# To find an average length of all the conversations
+average_conv_len = np.mean([len(x.split(" ")) for x in conversation])
+#print(average_conv_len)
+
+avg_length = 325
+
+# To pad shorter and truncate longer conversation
+vocabulary_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(avg_length)
+
+# To transform each conversation to numbers
+num_conversation = np.array(list(vocabulary_processor.fit_transform(conversation)))
+
+labels_array = np.array(labels)
+
+# To shuffle data
+shuffle = np.random.permutation(np.arange(len(num_conversation)))
+
+shuffled_conv = num_conversation[shuffle]
+shuffled_labels = labels_array[shuffle]
+
+training_size = 1200
+total_size = 1500
+
+train_conv = shuffled_conv[:training_size]
+train_labels = shuffled_labels[:training_size]
+
+test_conv = shuffled_conv[training_size:total_size]
+test_labels = shuffled_labels[training_size:total_size]
+
+tf.reset_default_graph()
+
+a = tf.placeholder(tf.int32, [None, avg_length])
+b = tf.placeholder(tf.int32, [None])
+
+batch_size = 20
+epochs = 15
+embedding = 50
+label_max = 2
+
+vocab_size = len(vocabulary_processor.vocabulary_)
+print(vocab_size)
+
+embedding_vocab = tf.Variable(tf.random_uniform([vocab_size, embedding], -1.0, 1.0))
+
+embed = tf.nn.embedding_lookup(embedding_vocab, a)
+print(embedding_vocab)
+print(embed)
+
+
+# To instantiate LSTM cells
+lstm = tf.contrib.rnn.BasicLSTMCell(embedding)
+
+# To prevent overfitting
+lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=0.80)
+
+output, (final_state, state_info) = tf.nn.dynamic_rnn(lstm, embed, dtype=tf.float32)
+
+print(final_state)
+
+logits = tf.layers.dense(final_state, label_max, activation=None)
+
+c_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=b)
+
+loss = tf.reduce_mean(c_entropy)
+
+forecast = tf.equal(tf.argmax(logits, 1), tf.cast(b, tf.int64))
+
+accuracy = tf.reduce_mean(tf.cast(forecast, tf.float32))
+
+
+# To use Adam optimizer to minimize the loss
+adam_opt = tf.train.AdamOptimizer(0.01)
+training_steps = adam_opt.minimize(loss)
+
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(epochs):
+        batches = int(len(train_conv) // batch_size) + 1
+        for i in range(batches):
+            minimum = i * batch_size
+            maximum = np.min([len(train_conv), ((i+1) * batch_size)])
+
+            x_train = train_conv[minimum:maximum]
+            y_train = train_labels[minimum:maximum]
+
+            training = {a: x_train, b:y_train}
+            sess.run(training_steps, feed_dict=training)
+
+            training_loss, training_accuracy = sess.run([loss, accuracy], feed_dict=training)
+
+        testing = {a: test_conv, b: test_labels}
+
+        test_loss, test_accuracy = sess.run([loss, accuracy], feed_dict=testing)
+        print("Epoch--> {}, Test Loss--> {:.3}, Test Accuracy--> {:.3}". format(epoch + 1, test_loss, test_accuracy))
